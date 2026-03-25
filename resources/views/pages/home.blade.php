e10fdd386139ae9f9b1d2773e395555de615f6f5
Ygor <ygor.stefan@gmail.com>
2026-03-24 21:42:20 -0300

    Corrigindo paginação sobrepondo cards de habilidades

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -127,26 +127,22 @@
             </div>
 
             {{-- Swiper carousel -- SKILL-01, SKILL-02, SKILL-03, SKILL-04 --}}
-            {{-- Pagination is a sibling of .swiper (not inside it) to avoid absolute-position overlap --}}
-            <div data-aos="fade-up" data-aos-delay="100">
-                <div class="swiper swiper-skills">
-                    <div class="swiper-wrapper">
-                        @foreach($skills as $skill)
-                            <div class="swiper-slide">
-                                <div class="bg-bg-primary border border-gray-800 rounded-xl p-6
-                                            flex flex-col items-center gap-3
-                                            hover:border-accent/50 hover:-translate-y-1
-                                            transition-all duration-300 cursor-default">
-                                    {{-- Devicon icon -- SKILL-02 --}}
-                                    <i class="{{ $skill['icon'] }} text-5xl"></i>
-                                    <span class="text-sm font-medium text-gray-300">{{ $skill['name'] }}</span>
-                                </div>
+            <div class="swiper swiper-skills" data-aos="fade-up" data-aos-delay="100">
+                <div class="swiper-wrapper">
+                    @foreach($skills as $skill)
+                        <div class="swiper-slide">
+                            <div class="bg-bg-primary border border-gray-800 rounded-xl p-6
+                                        flex flex-col items-center gap-3
+                                        hover:border-accent/50 hover:-translate-y-1
+                                        transition-all duration-300 cursor-default">
+                                {{-- Devicon icon -- SKILL-02 --}}
+                                <i class="{{ $skill['icon'] }} text-5xl"></i>
+                                <span class="text-sm font-medium text-gray-300">{{ $skill['name'] }}</span>
                             </div>
-                        @endforeach
-                    </div>
+                        </div>
+                    @endforeach
                 </div>
-                {{-- Swiper pagination outside .swiper so it flows naturally below cards --}}
-                <div class="swiper-pagination mt-6"></div>
+                <div class="swiper-pagination"></div>
             </div>
 
         </div>
